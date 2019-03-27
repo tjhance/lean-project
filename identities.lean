85020d8d56cb18f0b398c2a8a418b05f021d1b6f
Katherine Cordwell <kcordwel@andrew.cmu.edu>
2019-03-27 13:09:26 -0400

    removing redundant documentation

--- identities.lean
+++ identities.lean
@@ -10,28 +10,6 @@ import data.list.perm
    Katherine Cordwell (kcordwell)
 -/
 
-/- We prove various combinatorial identities.
-In particular,
-
- - Vandermonde's identity (Katherine's focus)
- - the Hockeystick identity (Katherine's focus)
- - the catalan identity: that the catalan numbers, as defined
-    by the usual recurrence, equals (1/(n+1))*(2n+1 choose n). (Travis's focus)
-
-For some or all of these, we will use combinatorial bijections.
-
-ORGANIZATION:
-    - identities.lean (this file):
-        - Theorems about set bijections
-        - Proof that the set of lists of length n with k
-          distinguished items has size (n choose k).
-    - hockeystick.lean:
-        - Proof of hockeystick identity and Vandermonde's identity
-    - catalan.lean
-        - Definition of catalan numbers by recurrence
-        - Proof that catalan numbers are (2n+1 choose n) / (2n+1)
--/
-
 /-
     Machinery for bijective proofs.
     `has_card` is a Prop that indicates a set is finite
