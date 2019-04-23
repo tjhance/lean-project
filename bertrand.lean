0f25a96e76a46d5e5866a745db6f3a797b1cb7ac
Travis Hance <tjhance7@gmail.com>
2019-04-23 13:44:10 -0400

    finish body of the main proof

--- bertrand.lean
+++ bertrand.lean
@@ -31,10 +31,16 @@ def primorial_bound : ∀ (n : ℕ) ,
     n ≥ 3 →
     8 * primorial n < 2 ^ (2*n) := sorry
 
+def range_to (n:ℕ) (m:ℕ) := list.range' n (m-n+1)
+
+theorem range_to_append : ∀ (n m k : ℕ) ,
+    n ≤ m → m ≤ k →
+    range_to n m ++ range_to (m+1) k = range_to n k := sorry
+
 theorem factorize : ∀ (n m : ℕ) ,
     (∀ p , prime p → p ∣ n → p ≤ m) →
     (∃ (r : ℕ → ℕ) , (∀ (p:ℕ) , prime p → p ^ r p ∣ n) ∧ 
-        n = (((list.range' 1 m).filter nat.prime).map
+        n = (((range_to 1 m).filter nat.prime).map
             (λ p , p ^ (r p))).prod
     ) := sorry
 
@@ -44,21 +50,22 @@ lemma p_le_2n_over_3 : ∀ (n p : ℕ) ,
     p ≤ (2*n / 3) := sorry
 
 lemma factorize_choose_2n_n : ∀ (n : ℕ) ,
+    (∀ (x : ℕ), prime x → n < x → 2 * n < x) →
     ∃ (r : ℕ → ℕ) ,
     (∀ (p:ℕ) , prime p → p ^ r p ∣ choose (2*n) n) ∧
     (choose (2*n) n =
-        (((list.range' 1 (2*n/3)).filter nat.prime).map
+        (((range_to 1 (2*n/3)).filter nat.prime).map
                 (λ p , p ^ (r p))).prod) := sorry
 
 theorem prime_bounds_1 : ∀ (n : ℕ) (r : ℕ → ℕ),
     (∀ p , prime p → p ^ r p ∣ (choose (2*n) n)) →
-    (((list.range' 1 (nat.sqrt (2*n))).filter nat.prime).map
+    (((range_to 1 (nat.sqrt (2*n))).filter nat.prime).map
                 (λ p , p ^ (r p))).prod
     ≤ (2*n) ^ (nat.sqrt (2*n)) := sorry
 
 theorem prime_bounds_2 : ∀ (n : ℕ) (r : ℕ → ℕ),
     (∀ p , prime p → p ^ r p ∣ (choose (2*n) n)) →
-    (((list.range' (nat.sqrt (2*n) + 1) (2*n/3)).filter nat.prime).map
+    (((range_to (nat.sqrt (2*n) + 1) (2*n/3)).filter nat.prime).map
                 (λ p , p ^ (r p))).prod
     ≤ primorial (2*n / 3) := sorry
 
@@ -66,40 +73,84 @@ theorem main_bound : ∀ (n : ℕ) ,
     n ≥ 468 →
     2*n * ((2*n) ^ (nat.sqrt (2*n))) * (4 ^ (2*n/3)) < 4^n := sorry
 
+lemma b_lt_c_of_ab_lt_c : ∀ (a b c : ℕ) ,
+    a > 0 → 
+    a*b < c → b < c := sorry
+
+lemma one_le_sqrt : ∀ (n : ℕ) ,
+    468 ≤ n → 1 ≤ nat.sqrt (2 * n) := sorry
+
+lemma sqrt_le_2_3 : ∀ (n : ℕ) ,
+    468 ≤ n → nat.sqrt (2 * n) ≤ 2 * n / 3 := sorry
+
+/- wtf why is this so hard? -/
+lemma obvious_calculation_312 : 312 = 2 * 468 / 3 := sorry
+
 lemma bertrands_postulate_main : ∀ (n : ℕ) ,
     n ≥ 468 → 
     ∃ p , prime p ∧ n < p ∧ p ≤ 2*n :=
 begin
     intros , by_contradiction , simp at a_1 ,
 
-    have h := factorize_choose_2n_n n ,
+    have h := factorize_choose_2n_n n a_1 ,
     cases h , rename h_w r , cases h_h ,
     rename h_h_left r_divides ,
     rename h_h_right choose_2n_n_factorization ,
 
     have t := (
     calc 4^n ≤ 2*n * (choose (2*n) n) : four_n_bound n
-    ... = 2*n * (((list.range' 1 (2*n/3)).filter nat.prime).map
+    ... = 2*n * (((range_to 1 (2*n/3)).filter nat.prime).map
                 (λ p , p ^ (r p))).prod : by rw choose_2n_n_factorization
     ... = 2*n *
-            (((list.range' 1 (nat.sqrt (2*n))).filter nat.prime).map
+            (((range_to 1 (nat.sqrt (2*n))).filter nat.prime).map
                 (λ p , p ^ (r p))).prod *
-            (((list.range' (nat.sqrt (2*n) + 1) (2*n/3)).filter nat.prime).map
-                (λ p , p ^ (r p))).prod : sorry
+            (((range_to (nat.sqrt (2*n) + 1) (2*n/3)).filter nat.prime).map
+                (λ p , p ^ (r p))).prod :
+            begin
+                rw [@nat.mul_assoc (2*n) _ _] ,
+                rw <- list.prod_append ,
+                rw <- list.map_append ,
+                rw <- list.filter_append ,
+                rw <- range_to_append ,
+                apply one_le_sqrt , assumption ,
+                apply sqrt_le_2_3 , assumption ,
+            end
+    ... ≤ 2*n *
+            (2*n) ^ (nat.sqrt (2*n)) *
+            (((range_to (nat.sqrt (2*n) + 1) (2*n/3)).filter nat.prime).map
+                (λ p , p ^ (r p))).prod :
+            begin
+                apply nat.mul_le_mul_right ,
+                apply nat.mul_le_mul_left ,
+                exact (prime_bounds_1 n r r_divides) ,
+            end
     ... ≤ 2*n *
             (2*n) ^ (nat.sqrt (2*n)) *
             primorial (2*n / 3) :
             begin
-                have pb1 := prime_bounds_1 n r r_divides ,
-                have pb2 := prime_bounds_2 n r r_divides ,
-                exact sorry
+                apply nat.mul_le_mul_left ,
+                exact (prime_bounds_2 n r r_divides) ,
             end
     ... ≤ 2*n *
             (2*n) ^ (nat.sqrt (2*n)) *
             4 ^ (2*n/3) :
             begin
-                have h := primorial_bound (2*n / 3),
-                exact sorry
+                have q := (calc
+                        (3:ℕ) ≤ 312 : (by linarith)
+                        ... = 2 * 468 / 3 : obvious_calculation_312
+                        ... ≤ 2 * n / 3 : begin
+                            apply nat.div_le_div_right ,
+                            apply nat.mul_le_mul_left ,
+                            assumption ,
+                        end
+                ),
+                have h := primorial_bound (2*n / 3) q,
+                have h' := b_lt_c_of_ab_lt_c _ _ _ (by linarith) h,
+                rw nat.pow_mul at h' ,
+                have h'' : (2 ^ 2 = 4) := by refl ,
+                rw h'' at h' ,
+                apply nat.mul_le_mul_left ,
+                apply le_of_lt , assumption ,
             end
     ... < 4^n : main_bound n a
     ),
